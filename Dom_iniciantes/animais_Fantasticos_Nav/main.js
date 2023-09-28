a6b99e7dea3bab42bd4f6ffd9ff1f121912aa84e
JuliaStella <julia.paula@liferay.com>
2023-09-28 17:44:40 -0300

    Em andamento o scroll suave

--- Dom_iniciantes/animais_Fantasticos_Nav/main.js
+++ Dom_iniciantes/animais_Fantasticos_Nav/main.js
@@ -39,3 +39,16 @@ function initAccordion() {
   }
 }
 initAccordion();
+
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+  event.preventDefault();
+  const href = event.currentTarget.getAttribute('href');
+  const section = document.querySelectorAll(href);
+  console.log(section);
+}
+
+linksInternos.forEach((link) => {
+  link.addEventListener('click', scrollToSection);
+})
