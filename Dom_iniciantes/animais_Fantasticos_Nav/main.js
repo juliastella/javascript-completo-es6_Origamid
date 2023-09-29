8bdbae10420ed01a0c8b0d21fdce4931440d409e
JuliaStella <julia.paula@liferay.com>
2023-09-29 15:06:23 -0300

    Scroll animaito wiht problem

--- Dom_iniciantes/animais_Fantasticos_Nav/main.js
+++ Dom_iniciantes/animais_Fantasticos_Nav/main.js
@@ -23,8 +23,8 @@ initTabNav();
 function initAccordion() {
   const accordionList = document.querySelectorAll('.js-accordion dt');
   const activeClass = 'ativo';
-  
-  if(accordionList.length) {
+
+  if (accordionList.length) {
     accordionList[0].classList.add(activeClass);
     accordionList[0].nextElementSibling.classList.add(activeClass);
 
@@ -40,15 +40,55 @@ function initAccordion() {
 }
 initAccordion();
 
-const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+function initScrollSuave(params) {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
 
-function scrollToSection(event) {
-  event.preventDefault();
-  const href = event.currentTarget.getAttribute('href');
-  const section = document.querySelectorAll(href);
-  console.log(section);
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+
+    // forma alternativa
+    // window.scrollTO({
+    //   top: topo,
+    //   behaviot: 'smooth',
+    // });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
 }
 
-linksInternos.forEach((link) => {
-  link.addEventListener('click', scrollToSection);
-})
+initScrollSuave();
+
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade;
+        if (isSectionVisible > 0) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
+    }
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
+}
+
+initAnimacaoScroll();
+
